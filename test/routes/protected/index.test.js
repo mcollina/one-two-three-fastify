d4150e143d3402810773be9c8eb9bd46101cd1c9
Alexandre Santos <ajvsms@gmail.com>
2020-12-12 17:04:23 +0000

    Add test for no token sent

---
+++ test/routes/protected/index.test.js
@@ -0,0 +1,18 @@
+import t from 'tap'
+import fastify from 'fastify'
+import fp from 'fastify-plugin'
+import app from '../../../app.js'
+
+const test = t.test
+
+test('no token provided for authentication', async (t) => {
+  const server = fastify()
+
+  // so we can access decorators
+  server.register(fp(app))
+
+  const res = await server.inject('/protected/something')
+  t.is(res.statusCode, 401)
+
+  await server.close()
+})
